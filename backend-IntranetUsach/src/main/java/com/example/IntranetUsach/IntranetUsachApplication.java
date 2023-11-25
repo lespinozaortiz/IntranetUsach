b27965c390c2dab48f61740b5fa408095e8ae471
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-11-25 01:48:36 -0300

    creación backend y front, inicialización

---
+++ backend-IntranetUsach/src/main/java/com/example/IntranetUsach/IntranetUsachApplication.java
@@ -0,0 +1,13 @@
+package com.example.IntranetUsach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetUsachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetUsachApplication.class, args);
+	}
+
+}
---
+++ backend-IntranetUsach/src/test/java/com/example/IntranetUsach/IntranetUsachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntranetUsach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetUsachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
