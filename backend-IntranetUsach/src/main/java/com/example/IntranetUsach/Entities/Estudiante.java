06a82af57be4038cf7b1c4940d899efa24eacd10
Leonardo Espinoza <leonardo.espinoza.o@usach.cl>
2023-11-30 20:40:27 -0300

    update estudiante

--- backend-IntranetUsach/src/main/java/com/example/IntranetUsach/Entities/Estudiante.java
+++ backend-IntranetUsach/src/main/java/com/example/IntranetUsach/Entities/Estudiante.java
@@ -1,4 +1,27 @@
 package com.example.IntranetUsach.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Estudiante {
-}
+    @Id
+    @Column(name = "rut", nullable = false, unique = true)
+    private Long rut;
+    private String nombres;
+    private String apellidos;
+
+
+
